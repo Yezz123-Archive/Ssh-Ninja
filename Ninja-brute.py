8138e23de853e6df69e7b74296e968485784e311
Yasser Tahiri <yasserth19@gmail.com>
2021-04-05 01:07:18 +0100

    usage

--- Ninja-brute.py
+++ Ninja-brute.py
@@ -38,7 +38,7 @@ def connect(host, user, password, release):
 def main():
 
     if len(argv) < 4:
-        print ('Usage: Ssh-Ninja.py host user passwords_list')
+        print ('Usage: Ninja-brute.py host user passwords_list')
         return
 
     host = argv[1]
